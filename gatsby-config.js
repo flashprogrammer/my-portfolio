f5667f7c064268a32837012b46ea9537a2b27727
unknown <noel.awitan@gmail.com>
2020-07-24 11:33:47 +0800

    Github build

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 const path = require(`path`)
 
 module.exports = {
+  pathPrefix: "/my-portfolio",
   siteMetadata: {
     title: `LA Portfolio`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
