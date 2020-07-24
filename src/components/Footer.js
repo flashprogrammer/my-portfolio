67928d2d82a18c6a852a801aa1fa0e42bf26f1c6
unknown <noel.awitan@gmail.com>
2020-07-24 11:20:40 +0800

    Footer component. Add social media icons and css in Footer component. Removed unsued scss files.

---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { FaFacebookF, FaInstagram, FaGoogle, FaStrava } from "react-icons/fa"
+
+const Footer = () => (
+    <div className="site-footer">
+        <div className="footer-social-links">
+            <ul className="social-links-list">
+                <a className="facebook" href="https://www.facebook.com/noelits03" target="_blank">
+                    <FaFacebookF/>
+                </a>
+                <a className="instagram" href="https://www.instagram.com/la.renzz/" target="_blank">
+                    <FaInstagram />
+                </a>
+                <a className="google" href="mailto:noel.awitan@gmail.com">
+                    <FaGoogle />
+                </a>
+                <a className="strava" href="https://www.strava.com/athletes/51606379" target="_blank">
+                    <FaStrava/>
+                </a>
+            </ul>
+        </div>
+        <span> &#169; 2019 - 2020 Noel Lawrence Awitan</span>
+    </div>
+)
+
+export default Footer
